95f1c4ec05322ccddaa0df24b4c32ab1c3bbebf4
Marc Veens <marcveens@hotmail.com>
2020-12-27 15:17:15 +0100

    Initial

---
+++ src/components/app-root/app-root.tsx
@@ -0,0 +1,16 @@
+import { Component, h } from '@stencil/core';
+
+@Component({
+    tag: 'app-root'
+})
+export class AppRoot {
+    render() {
+        return (
+            <div>
+                <main>
+                    Hey you
+                </main>
+            </div>
+        );
+    }
+}
